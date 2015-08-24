2b91d687a96ea356dd9dd87ebf9c428efd61be97
nharringtonwasatch <nharrington@wasatchphotonics.com>
2015-08-24 09:17:31 -0400

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# WasatchAnalyzeIQ
+WasatchAnalyzeIQ
+=========
+
+Acquire data from Wasatch Photonics spectrometers for use in AnalyzeIQ
+
+
+Requirements
+------------
+
+  * PyQt 4.7+
+  * NumPy
+
+Installation Methods
+--------------------
+
+Run windows installer
+Copy of the analyzeiq required configuration
+
+Documentation
+-------------
+
+* First, run the tests with nose
+
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    "description": " Acquire data from Wasatch Photonics spectrometers for use in AnalyzeIQ",
+    "author": "Nathan Harrington",
+    "url": "https://github.com/nharringtonwasatch/WasatchAnalyzeIQ",
+    "download_url": "https://github.com/nharringtonwasatch/WasatchAnalyzeIQ",
+    "author_email": "nharrington@wasatchphotonics.com.",
+    "version": "1.0.0",
+    "install_requires": ["numpy", "PyQt4"],
+    "packages": ["WasatchAnalyzeIQ"],
+    "scripts": [],
+    "name"; "WasatchAnalyzeIQ"
+}
+
+setup(**config)
