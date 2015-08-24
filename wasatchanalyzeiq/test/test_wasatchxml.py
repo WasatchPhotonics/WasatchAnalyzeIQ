48600353e4e5e052f90e7a43cb45f338e3c07e81
nharringtonwasatch <nharrington@wasatchphotonics.com>
2015-08-24 10:03:56 -0400

    show required in mainwindow for tests to pass

--- wasatchanalyzeiq/test/test_wasatchxml.py
+++ wasatchanalyzeiq/test/test_wasatchxml.py
@@ -19,14 +19,23 @@ class Test(unittest.TestCase):
 
     def test_startup_layout(self):
         # window is correct size
-        self.assertEqual(self.form.width(), 800)
-        self.assertEqual(self.form.height(), 600)
+        self.assertEqual(self.form.width(), 496)
+        self.assertEqual(self.form.height(), 302)
         
         # Basic controls are available
+        fui = self.form.ui
+        self.assertTrue(fui.checkBoxLaserEnable.isVisible())
+        self.assertTrue(fui.checkBoxLaserEnable.isEnabled())
 
         # Lcd number is invisible
+        self.assertFalse(self.form.ui.lcdNumber.isVisible())
 
-    #def test_control_limits(self):
+    def test_control_limits(self):
+        fui = self.form.ui
+
+        # Laser enable starts off unchecked
+        self.assertFalse(fui.checkBoxLaserEnable.isChecked())
+    
     #def test_xml_output(self):
     #def test_long_integration_feedback(self):
 if __name__ == "__main__":
